c71c51fe263496dcc0a0bf7cab6c522f727f779f
Paisaa <edupaesano@gmail.com>
2025-07-07 10:25:24 -0300

    add index.php
    utilizemos por ahora ese archivo para redireccionar a lo que estemos trabajando y las diferentes carpetas

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Redireccionar a Login</title>
+</head>
+<body>
+    <form action="app/views/login.php" method="get">
+        <button type="submit">Ir a "Login.php"</button>
+    </form>
+</body>
+</html></body>
